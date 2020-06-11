f825864d24ac999639c7e70e3e1fbe7923e72619
awsky <969748465@qq.com>
2020-06-11 09:35:47 +0800

    提交代码

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,24 @@ import {
     useParams
 } from "react-router-dom";
 
-import Header from '@/views/layout/Header';
+import Header from '@/views/layout/Header/Header';
 import SlideBar from '@/views/layout/SlideBar';
 import Main from '@/views/layout/Main';
+
+import styles from './app.modules.scss';
 
 export default function App() {
     return (
         <Router>
-            <div className="container">
-                <Header />
+            <div className={styles.container}>
+                <SlideBar />
 
-                <div className="main">
-                    <SlideBar />
+                <div className={styles.main}>
+                    <Header />
 
                     <Main />
                 </div>
             </div>
-            
-            
         </Router>
     );
 }
