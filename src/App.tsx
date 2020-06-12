1444bf37a91f63afe4b8ad30271197ac8b4960a8
awsky <969748465@qq.com>
2020-06-12 08:47:33 +0800

    配置代码格式化

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-
-                {
-                    process.env.REACT_APP_DOMAIN
-                }
-            </header>
-        </div>
-    );
+    return <div className={styles.App}>123</div>;
 }
 
 export default App;
---
+++ src/views/layout/Main.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = function() {
+    return <span>Main</span>;
+}
+
+export default Main;
